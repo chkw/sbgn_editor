4f6ce7a4c05629a095713d265c4d22438f7ed872
chrisw <chrisw@soe.ucsc.edu>
2013-09-24 15:43:40 -0700

    update to current version

--- scripts/graphData.js
+++ scripts/graphData.js
@@ -6,8 +6,19 @@ function nodeData(data) {
     if ('group' in data) {
         this.group = data['group'];
     } else {
-        this.group = 'protein';
+        this.group = 'unspecified entity';
     }
+
+    /**
+     * Check if this nodeData is equal to the specified nodeData.
+     */
+    this.checkEquality = function(otherNodeData) {
+        if (this.name == otherNodeData.name && this.group == otherNodeData.group) {
+            return true;
+        } else {
+            return false;
+        }
+    };
 }
 
 /**
@@ -19,7 +30,10 @@ function linkData(data) {
     if ('value' in data) {
         this.value = parseFloat(data['value']);
     } else {
-        this.value = 1;
+        this.value = 3;
+    }
+    if ('relation' in data) {
+        this.relation = data['relation'];
     }
 }
 
@@ -30,23 +44,60 @@ function graphData() {
     this.nodes = new Array();
     this.links = new Array();
 
-    this.addNode = function(nodeData) {
-        if (nodeData.constructor.name == 'nodeData') {
-            this.nodes.push(nodeData);
-        } else {
-            console.log('not adding node: ' + JSON.stringify(nodeData));
+    /**
+     * Get all the node names in the graph.
+     */
+    this.getAllNodeNames = function() {
+        var nodeNames = new Array();
+        for (var i in this.nodes) {
+            var nodeData = this.nodes[i];
+            var nodeName = nodeData['name'];
+            nodeNames.push(nodeName);
         }
-    }
+        return nodeNames;
+    };
+
+    /**
+     * Add a node to the graph.
+     */
+    this.addNode = function(nodeData) {
+        // check if it is nodeData object
+        if (nodeData.constructor.name != 'nodeData') {
+            console.log('not nodeData: ' + JSON.stringify(nodeData));
+            return null;
+        }
+
+        // check if node already exists
+        var exists = false;
+        for (var i in this.nodes) {
+            var node = this.nodes[i];
+            if (node.checkEquality(nodeData)) {
+                console.log('nodeData exists');
+                exists = true;
+                break;
+            }
+        }
+
+        if (!exists) {
+            // add node
+            this.nodes.push(nodeData);
+            return nodeData;
+        } else {
+            return null;
+        }
+    };
     /**
      * Does not check if both source and target nodes exist.
      */
     this.addLink = function(linkData) {
+        // TODO first, check if link exists
         if (linkData.constructor.name == 'linkData') {
             this.links.push(linkData);
         } else {
             console.log('not adding link: ' + JSON.stringify(linkData));
         }
-    }
+    };
+
     /**
      * Get IDs for nodes that have the specified name.
      */
@@ -60,11 +111,12 @@ function graphData() {
             }
         }
         return idList;
-    }
+    };
     /**
      * Delete a node by the name.
      */
     this.deleteNodeByName = function(name) {
+        // TODO deleting node should force re-indexing of link source/targets
         // nothing to delete
         if (this.nodes.length < 1) {
             console.log('no nodes to delete');
@@ -107,7 +159,60 @@ function graphData() {
         }
         node = this.nodes[idx];
         removeA(this.nodes, node);
-    }
+    };
+    /**
+     * read graph links from TAB text
+     */
+    this.readTab = function(text) {
+        // clear old graph
+        this.nodes = new Array();
+        this.links = new Array();
+
+        var lines = text.split('\n');
+
+        // nodes
+        var nodeNameArray = new Array();
+        for (var i in lines) {
+            var fields = lines[i].split('\t');
+            if (fields.length >= 2) {
+                var sourceName = fields[0];
+                var targetName = fields[1];
+                nodeNameArray.push(sourceName);
+                nodeNameArray.push(targetName);
+            }
+        }
+        nodeNameArray = d3.set(nodeNameArray).values();
+        for (var i in nodeNameArray) {
+            var nodeName = nodeNameArray[i];
+            this.addNode(new nodeData({
+                name : nodeName
+            }));
+        }
+
+        // links
+        for (var i in lines) {
+            var fields = lines[i].split('\t');
+            if (fields.length >= 2) {
+                var sourceName = fields[0];
+                var targetName = fields[1];
+                var relation = '';
+                if (fields.length >= 3) {
+                    relation = fields[2];
+                } else {
+                    relation = 'unspecified';
+                }
+
+                var sourceIdxList = this.getNodeIdsByName(sourceName);
+                var targetIdxList = this.getNodeIdsByName(targetName);
+
+                this.addLink(new linkData({
+                    sourceIdx : sourceIdxList[0],
+                    targetIdx : targetIdxList[0],
+                    'relation' : relation
+                }));
+            }
+        }
+    };
     /**
      * read graph SIF text
      */
@@ -155,7 +260,7 @@ function graphData() {
                 }));
             }
         }
-    }
+    };
     /**
      * read graph from PID text
      */
@@ -203,12 +308,39 @@ function graphData() {
                     this.addLink(new linkData({
                         'sourceIdx' : parseInt(sourceIdx),
                         'targetIdx' : parseInt(targetIdx),
-                        value : 3,
+                        'relation' : relation
                     }));
                 }
             }
         }
-    }
+    };
+
+    /**
+     * Get the graph as a PID string.  Bug: Nodes that have same name, different group/type will give possibly unexpected results in the relations section.
+     */
+    this.toPid = function() {
+        var pidString = '';
+
+        // nodes
+        for (var i in this.nodes) {
+            var node = this.nodes[i];
+            var nodeString = node['group'] + '\t' + node['name'] + '\n';
+            pidString = pidString + nodeString;
+        }
+
+        // relations
+        for (var i in this.links) {
+            var link = this.links[i];
+            var relation = link['value'];
+            if ('relation' in link) {
+                relation = link['relation'];
+            }
+            var linkString = link['source']['name'] + '\t' + link['target']['name'] + '\t' + relation + '\n';
+            pidString = pidString + linkString;
+        }
+
+        return pidString;
+    };
 }
 
 /**
